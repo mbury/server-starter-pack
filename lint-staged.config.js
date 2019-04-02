07d1bcd80418c141bd2fa2425e05eeeca7174d34
Michał Bury <michalbury@hotmail.com>
2019-04-02 16:29:44 +0200

    Add prettier with script and lint staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   linters: {
-    '**/*.+(js|json|less|css|ts|tsx|md)': [
-      'jest --findRelatedTests',
-      'git add',
-    ],
+    '**/*.+(js|json|less|css|ts|tsx|md)': ['prettier --write', 'git add'],
+    '*.js': ['jest --findRelatedTests', 'eslint --fix', 'git add'],
   },
 };
